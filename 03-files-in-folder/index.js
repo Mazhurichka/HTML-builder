b34c248b6128bdcb2dfccb956e24606a40900c11
Maryna Matsypula <Mazhurichka@gmail.com>
2022-05-25 21:51:39 +0300

    feat: fix import files

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,9 +1,5 @@
 const path = require('path')
-const dirPath = path.join(__dirname, '/03-files-in-folder/secret-folder')
 const fs = require('fs')
-const readline = require('readline')
-const process = require('process')
-// const promices = require('fs').promises
 
 fs.readdir(
   './03-files-in-folder/secret-folder/',
