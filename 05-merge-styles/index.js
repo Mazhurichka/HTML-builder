2e488c8c20d93829f9fdd895b16557034196fa92
Maryna Matsypula <Mazhurichka@gmail.com>
2022-05-26 17:32:07 +0300

    feat: add bundle.css

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,50 +1,75 @@
 const path = require('path')
 const fs = require('fs')
 
+fs.readdir(
+  __dirname,
+  (arr, data) => {
+    data.forEach((file) => {
+      fs.stat(__dirname + '/' + file, (err, stats) => {
+        if (err) {
+          throw err
+        }
 
-  fs.readdir(
-    __dirname,
-    (arr, data) => {
+        if (stats.isDirectory(file)) {
+          fs.readdir(__dirname + '/' + file, (err, filesInOtherFolder) => {
+            if (err) throw err
 
-      data.forEach((file) => {
-        fs.stat(__dirname + '/' + file, (err, stats) => {
-          if (err) {
-            throw err
-          }
+            filesInOtherFolder.forEach((file) => {
+              //console.log(file)
+              if (file.includes('.css') && file !== 'bundle.css') {
+                fs.readFile(
+                  __dirname + '/styles/' + file,
+                  'utf8',
+                  (err, data) => {
+                    if (err) throw err
 
-          if (stats.isDirectory(file)) {
-            fs.readdir(__dirname + '/' + file, (err, filesInOtherFolder) => {
-              if (err) throw err
+                    fs.appendFile(
+                      __dirname + '/project-dist/bundle.css',
+                      data,
+                      (err) => {
+                        if (err) throw err
 
-              filesInOtherFolder.forEach((file) => {
-                //console.log(file)
-                if(file.includes('.css') && file !== 'bundle.css') {
-                  fs.readFile(
-                    __dirname + '/styles/' + file,
-                    'utf8',
-                    (err, data) => {
-                      if (err) throw err
-
-                      fs.appendFile(
-                        __dirname + '/project-dist/bundle.css',
-                        data,
-                         (err) => {
-                          if(err) throw err;
-                          console.log('files marged')
-                        }
-                      )
-                    }
-                  )
-                }
-
-               
-                
-              })
+                        console.log('file marged')
+                      }
+                    )
+                  }
+                )
+              }
             })
-          }
-        })
-      })
-    },
-    { withFileTypes: true }
-  )
+          })
 
+          if (file === 'test-files') {
+            fs.readdir(
+              __dirname + '/' + file + '/styles/',
+              (err, filesInOtherFolder) => {
+                if (err) throw err
+
+                filesInOtherFolder.forEach((cssFile) => {
+                  if (stats.isDirectory(file)) {
+                    fs.readFile(
+                      __dirname + '/' + file + '/styles/' + cssFile,
+                      'utf8',
+                      (err, data) => {
+                        if (err) throw err
+
+                        fs.appendFile(
+                          __dirname + '/project-dist/bundle.css',
+                          data,
+                          (err) => {
+                            if (err) throw err
+                            console.log('file marged')
+                          }
+                        )
+                      }
+                    )
+                  }
+                })
+              }
+            )
+          }
+        }
+      })
+    })
+  },
+  { withFileTypes: true }
+)
